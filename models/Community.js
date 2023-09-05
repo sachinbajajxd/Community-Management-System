ff9451a617d612df4645e31f8f40f2ae83712a39
sachinbajajxd <112001510+sachinbajajxd@users.noreply.github.com>
2023-09-05 12:03:18 +0530

    Updated

--- models/Community.js
+++ models/Community.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 const User=require('./User');
 
-// Define the Snowflake library for generating IDs
-const Snowflake = require('@theinternetfolks/snowflake'); // Replace with the actual library
 
 // Create a Mongoose schema
 const communitySchema = new mongoose.Schema({
   id: {
     type: String,
-    default: () => Snowflake.generate(), // Generate Snowflake ID as the default value
     primary: true, // Indicate that this is the primary key
   },
   name: {
